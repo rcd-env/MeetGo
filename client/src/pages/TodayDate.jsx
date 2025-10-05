4edeadf26e046ddaee77da0de7921606fce23c11
rcd-env <donerakesh001@gmail.com>
2025-10-05 15:35:03 +0530

    add TodayDate component to display the current date

---
+++ client/src/pages/TodayDate.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+function TodayDate() {
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => setDate(new Date()), 1000 * 60 * 60);
+    return () => clearInterval(timer);
+  }, []);
+  const formattedDate = date.toLocaleDateString("en-US", {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+  });
+  return <p className="ml-2 mr-4">{formattedDate}</p>;
+}
+
+export default TodayDate;
