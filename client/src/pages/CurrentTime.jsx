1f8718270e3e24a280ece19e7e82bf770913675e
rcd-env <donerakesh001@gmail.com>
2025-10-05 15:35:15 +0530

    add CurrentTime component to display the current time

---
+++ client/src/pages/CurrentTime.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const CurrentTime = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date()), 1000); // update every second
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+
+  return <p className="mr-2">{formattedTime}</p>;
+};
+
+export default CurrentTime;
